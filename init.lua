4535657a6d650948148859da14ec5ae76a818826
aryan-think <aryan.kamboj@think41@gmail.com>
2025-04-14 18:39:01 +0530

    linux ready

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- Load packer if installed
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- Add your plugins here, for example:
+  -- use 'nvim-treesitter/nvim-treesitter'
+end)
+
 require("aryan")
 require("aryan.packer")
 require("autoclose").setup()
